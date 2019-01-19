1208d53f23af38c7e43dce32747967ac06975824
Sandeep Shivajirao <sndy.chvn.fr@gmail.com>
2019-01-19 22:33:30 +0100

    User Input Configuration

--- output_config.py
+++ output_config.py
@@ -5,20 +5,22 @@ class Count:
 
     def yimage_conf_out(final):
 
-        object_name = "motorbike"
+        object_name = ['motorbike', 'person', 'bus', 'truck', 'bicycle', 'car']
 
-        print(final)
-        file1 = open("Object_Number.txt", "r+")
-        obj9 = file1.read().split()
-        #print(obj9.index(object_name))
-        ind = obj9.index(object_name)
-        #print(obj9[final[0]])
-        print(obj9[ind])
-        cobj = Counter(final)
-        print(cobj[ind])
-        #print(cobj[final[0]])
+        for vari in object_name:
+            #print(final)
+            file1 = open("Object_Number.txt", "r+")
+            obj9 = file1.read().split()
+            # print(obj9.index(object_name))
+            ind = obj9.index(vari)
+            # print(obj9[final[0]])
+            print(obj9[ind])
+            cobj = Counter(final)
+            print(cobj[ind])
+            # print(cobj[final[0]])
 
-        #if (obj9[final[0]])
+            # if (obj9[final[0]])
+
 
 
 
